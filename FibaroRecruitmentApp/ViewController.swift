ec08f7b336f8d3eb75db8d1f09a9a0526474abd5
Dominika Czarnecka <dominika.czarnecka@icloud.com>
2018-02-24 09:17:15 +0100

    Initial Commit

---
+++ FibaroRecruitmentApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FibaroRecruitmentApp
+//
+//  Created by Dominika Czarnecka on 24.02.2018.
+//  Copyright © 2018 Dominika Czarnecka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
